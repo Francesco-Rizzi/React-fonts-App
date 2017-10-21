1b7547e07cd6f93da0561a7a9958482ab589e856
Francesco Rizzi <rizzifrancesco@icloud.com>
2017-10-21 19:07:16 +0200

    RELATED BRANCH: master; MESSAGE: prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -16,4 +18,5 @@ module.exports = {
       }
     }]
   },
+	plugins : [ new webpack.DefinePlugin({'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)}) ]
 };
